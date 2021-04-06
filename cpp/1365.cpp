55c35cab6bb3beaaccd63dbbea7aa124d6e9ed15
coderaky <45727674+coderaky@users.noreply.github.com>
2021-04-06 18:13:13 +0530

    modified 1365

--- cpp/1365.cpp
+++ cpp/1365.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
-        vector<int> hash(102,0);
+        vector<int> hash(101);        
         for(auto i:nums) hash[i]++;
-        for(int i=1;i<102;i++) hash[i]+=hash[i-1];
-        for(int i=0;i<nums.size();i++) nums[i]=hash[nums[i]-1];
+        for(int i=1;i<101;i++) hash[i]+=hash[i-1];
+        for(int i=0;i<nums.size();i++) nums[i]=nums[i]?hash[nums[i]-1]:0;      
         return nums; 
         }
 };
