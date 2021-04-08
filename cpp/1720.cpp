7207e74aa5c6548d3c6f6a0fc9511ff0944de5be
coderaky <45727674+coderaky@users.noreply.github.com>
2021-04-08 23:07:23 +0530

    modiefied 1720

--- cpp/1720.cpp
+++ cpp/1720.cpp
@@ -1,14 +1,10 @@
 class Solution {
 public:
     vector<int> decode(vector<int>& encoded, int first) {
-        vector<int> res;
-        int temp;
-        res.push_back(first);
-        for(auto i: encoded){
-            temp=i^first;
-            res.push_back(temp);
-            first=temp;
-        }
+        vector<int> res(encoded.size()+1);
+        res[0]=first;
+        for(int i=0;i<encoded.size();i++)
+            res[i+1]=res[i]^encoded[i];
         return res;
     }
 };
