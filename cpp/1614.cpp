3587de1061920434c4589fae241568015462b213
coderaky <45727674+coderaky@users.noreply.github.com>
2021-04-17 07:00:25 +0530

    modiefied 1614

--- cpp/1614.cpp
+++ cpp/1614.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     int maxDepth(string s) {
-        int cd=0,md=0;
-        for(auto i: s){
-            if(i=='(') cd++;
-            if(i==')') cd--;
-            md=max(md,cd);
+        int c=0,d=0;
+        for(auto i:s){
+            c+=i=='('?1:i==')'?-1:0;
+            d=max(d,c);
         }
-        return md;
+        return d;
     }
 };
