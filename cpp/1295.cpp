4e47bbc8f3d23bbfcf3623f27fc004c047507db3
coderaky <45727674+coderaky@users.noreply.github.com>
2021-04-21 17:38:30 +0530

    modified 1295

--- cpp/1295.cpp
+++ cpp/1295.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int findNumbers(vector<int>& nums) {
         int c=0;
-        for(auto i: nums) if((int)floor(log10(i) + 1)%2==0) c++;
+        for(auto i: nums) if(!((int)floor(log10(i) + 1) & 1)) c++;
         return c;
     }
 };
