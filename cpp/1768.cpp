6ed63557ca34a13342ac9b8bffd69d09b2bca9bd
coderaky <45727674+coderaky@users.noreply.github.com>
2021-04-26 17:51:56 +0530

    1768

---
+++ cpp/1768.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        int i=0,j=0;
+        string res="";
+        while(i<word1.size() || j<word2.size()){
+            if(i<word1.size())res+=word1[i++];
+            if(j<word2.size())res+=word2[j++];
+        }
+        return res;
+    }
+};
