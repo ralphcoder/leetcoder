488aae0e818dcdce5d7f909b4ae351b459de19d2
coderaky <45727674+coderaky@users.noreply.github.com>
2021-04-26 13:52:41 +0530

    1450

---
+++ cpp/1450.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int busyStudent(vector<int>& startTime, vector<int>& endTime, int queryTime) {
+        int c=0;
+        for(int i=0;i<startTime.size();i++)
+            c+= startTime[i]<=queryTime && endTime[i]>=queryTime ;
+        return c;
+    }
+};
