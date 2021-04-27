aee9bd393d3cca9f0dce1feceaa6f825adf9fd60
coderaky <45727674+coderaky@users.noreply.github.com>
2021-04-27 14:01:00 +0530

    961

--- cpp/961.cpp
+++ cpp/961.cpp
@@ -2,10 +2,8 @@ class Solution {
 public:
     int repeatedNTimes(vector<int>& A) {
         unordered_map<int,int> hash;
-        for(auto i:A){
-            hash[i]++;
-            if(hash[i]>1)return i;
-        }
+        for(auto i:A)
+            if(++hash[i]>1)return i;
         return 0;
     }
 };
