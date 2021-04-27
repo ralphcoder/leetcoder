43a5d6984d6265ab1be2efe1f84647e1f3ace76a
coderaky <45727674+coderaky@users.noreply.github.com>
2021-04-27 14:37:55 +0530

    1299

--- cpp/1299.cpp
+++ cpp/1299.cpp
@@ -1,14 +1,9 @@
 class Solution {
 public:
     vector<int> replaceElements(vector<int>& arr) {
-        int n=arr.size()-1;
-        vector<int> res (arr.size());
-        int g=arr[n];
-        res[n]=-1;
-        for(int i=arr.size()-1;i>0;i--){
-            g=max(arr[i],g);
-            res[--n]=g;
-        }
-        return res;
+        int m=-1;
+        for(int i=arr.size()-1;i>=0;i--) 
+            m=max(m,exchange(arr[i],m));
+        return arr;
     }
 };
