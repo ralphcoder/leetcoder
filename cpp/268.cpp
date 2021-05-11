17e0a1d181bd200e895663e6e33d0ec4fc71db3f
coderaky <45727674+coderaky@users.noreply.github.com>
2021-05-11 20:14:21 +0530

    268

---
+++ cpp/268.cpp
@@ -0,0 +1,7 @@
+// C++ | One-liner using Gauss Formula
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        return nums.size()*(nums.size()+1)/2-accumulate(nums.begin(),nums.end(),0);
+    }
+};
