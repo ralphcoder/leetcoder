4a12b939a2ad417532b7e03e2e1ada4872fc4c5d
coderaky <45727674+coderaky@users.noreply.github.com>
2021-05-14 10:28:02 +0530

    94

--- cpp/26.cpp
+++ cpp/26.cpp
@@ -17,3 +17,15 @@ public:
         return j;
     }
 };
+
+//Little more cleaner approach
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums,int c=0) {
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]==nums[i-1])c++;
+            else nums[i-c]=nums[i];
+          }
+        return nums.size()-c;
+    }
+};
