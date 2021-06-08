ae98b056c8484a6fc3923048a47d49ab9b1780b4
coderaky <45727674+coderaky@users.noreply.github.com>
2021-06-08 16:05:27 +0530

    7

---
+++ cpp/7.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int rev=0;
+        while(x!=0){
+            int r=x%10;
+            if(rev>INT_MAX/10 || (rev==INT_MAX/10 && r>7))return 0;
+            if(rev<INT_MIN/10 || (rev==INT_MIN/10 && r<-8))return 0;
+            rev=rev*10+r;
+            x/=10;
+        }
+        return rev;
+    }
+};
