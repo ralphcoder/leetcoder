2d7d83e9b47785e1aa3366ebbdc4bc007431f517
coderaky <45727674+coderaky@users.noreply.github.com>
2021-07-16 15:56:55 +0530

    88

---
+++ cpp/88.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i=m-1,j=n-1,t=m+n-1;
+        while(j>=0) nums1[t--]=i>=0 && nums1[i]>nums2[j]?nums1[i--]:nums2[j--];
+    }
+};
