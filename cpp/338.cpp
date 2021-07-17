2a8bdca62891d1cad38062dd8e1f868812f48e13
coderaky <45727674+coderaky@users.noreply.github.com>
2021-07-17 14:00:36 +0530

    338

---
+++ cpp/338.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> res;
+        for(int i=0;i<=n;i++)
+            res.push_back(__builtin_popcount(i));
+        return res;
+    }
+};
