9acf76e7d500d2f71274a842abcd4f715711d142
coderaky <45727674+coderaky@users.noreply.github.com>
2021-09-01 10:42:16 +0530

    2

--- cpp/9.cpp
+++ cpp/9.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     bool isPalindrome(int x) {
         long d=x,s=0;
-        while(d!=0){
+        while(d>0){
             s=s*10+d%10;
             d/=10;
         }
-        return s==x && x>=0?true:false;
+        return s==x;
     }
 };
